ab00d85a078a14b37753768ec42099baa00e5013
shadhin <md.tushar_91@yahoo.com>
2015-09-19 02:21:00 +0600

    basic structure updated

---
+++ blog/app.js
@@ -0,0 +1,53 @@
+angular.module('creditworldApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate','ui.materialize',
+               //'ui.bootstrap', // for modal dialogs
+    'ngResource', 'ui.router', 'ngCookies', 'ngCacheBuster', 'ngFileUpload', 'infinite-scroll' ])
+
+    .run(function ($rootScope, $location, $window, $http, $state, $translate, Language, Auth, Principal, ENV, VERSION) {
+        $rootScope.ENV = ENV;
+        $rootScope.VERSION = VERSION;
+        $rootScope.$on('$stateChangeStart', function (event, toState, toStateParams) {
+            $rootScope.toState = toState;
+            $rootScope.toStateParams = toStateParams;
+
+            if (Principal.isIdentityResolved()) {
+                Auth.authorize();
+            }
+
+            // Update the language
+            Language.getCurrent().then(function (language) {
+                $translate.use(language);
+            });
+
+        });
+
+        $rootScope.$on('$stateChangeSuccess',  function(event, toState, toParams, fromState, fromParams) {
+            var titleKey = 'global.title' ;
+
+            $rootScope.previousStateName = fromState.name;
+            $rootScope.previousStateParams = fromParams;
+
+            // Set the page title key to the one configured in state or use default one
+            if (toState.data.pageTitle) {
+                titleKey = toState.data.pageTitle;
+            }
+
+            $translate(titleKey).then(function (title) {
+                // Change window title with translated one
+                $window.document.title = title;
+            });
+
+        });
+
+        $rootScope.back = function() {
+            // If previous state is 'activate' or do not exist go to 'home'
+            if ($rootScope.previousStateName === 'activate' || $state.get($rootScope.previousStateName) === null) {
+                $state.go('home');
+            } else {
+                $state.go($rootScope.previousStateName, $rootScope.previousStateParams);
+            }
+        };
+
+        $rootScope.isActive = function(state) {
+            return state === $location.path();
+        };
+    });
---
+++ blog/config/config.js
@@ -0,0 +1,32 @@
+angular.module('creditworldApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate','ui.materialize',
+    //'ui.bootstrap', // for modal dialogs
+    'ngResource', 'ui.router', 'ngCookies', 'ngCacheBuster', 'ngFileUpload', 'infinite-scroll' ])
+
+.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider, $translateProvider, tmhDynamicLocaleProvider, httpRequestInterceptorCacheBusterProvider) {
+
+    //Cache everything except rest api requests
+    httpRequestInterceptorCacheBusterProvider.setMatchlist([/.*api.*/, /.*protected.*/], true);
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('site', {
+            'abstract': true,
+            views: {
+                'navbar@': {
+                    template: '<h1>this is navbar</h1>',
+                    controller: ''
+                },
+                'sidebar@': {
+                    template: '<h1>this is sidebar</h1>',
+                    controller: ''
+                },
+                'content@': {
+                    template: '<h1>this is main content</h1>',
+                    controller: ''
+                },
+                'footer@': {
+                    template: '<h1>this is footer</h1>',
+                    controller: ''
+                }
+            }
+        });
+});
